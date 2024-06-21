1fed5bb0fa11f0e29cad815951e6397d915af57a
Kienan <69349336+KienanUnique@users.noreply.github.com>
2024-06-22 01:46:07 +0500

    TKM-124 fix player rotation

--- Assets/Scripts/Services/ScreenPosition/Impl/ScreenPositionService.cs
+++ Assets/Scripts/Services/ScreenPosition/Impl/ScreenPositionService.cs
@@ -6,7 +6,9 @@ namespace Services.ScreenPosition.Impl
     {
         public Vector2 ConvertScreenPositionToWorld(Vector2 screenPosition)
         {
-            return Camera.main.ScreenToWorldPoint(screenPosition);
+            var camera = Camera.main;
+            var newScreenPosition = new Vector3(screenPosition.x, screenPosition.y, -camera.transform.position.z);
+            return camera.ScreenToWorldPoint(newScreenPosition);
         }
     }
 }
