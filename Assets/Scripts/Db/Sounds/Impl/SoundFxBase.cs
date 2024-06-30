6d09eb79b2d78f0e692bec53537910093f2686c2
Kienan <69349336+KienanUnique@users.noreply.github.com>
2024-07-01 02:42:17 +0500

    TKM-118 add sound fx base

---
+++ Assets/Scripts/Db/Sounds/Impl/SoundFxBase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Alchemy.Inspector;
+using Alchemy.Serialization;
+using UnityEngine;
+using Utils;
+using Utils.Sounds;
+
+namespace Db.Sounds.Impl
+{
+    [AlchemySerialize]
+    [CreateAssetMenu(menuName = MenuPathBase.Parameters + nameof(SoundFxBase), fileName = nameof(SoundFxBase))]
+    public partial class SoundFxBase : ScriptableObject
+    {
+        [TabGroup("Tab", "Game")] 
+        [AlchemySerializeField, NonSerialized]
+        private Dictionary<EGameSoundFxType, AudioClipVo> gameSounds = new();
+
+        [TabGroup("Tab", "Ui")] 
+        [AlchemySerializeField, NonSerialized]
+        private Dictionary<EUiSoundFxType, AudioClipVo> uiSounds = new();
+
+        [TabGroup("Tab", "Music")] 
+        [AlchemySerializeField, NonSerialized]
+        private List<AudioClipVo> backgroundMusic = new();
+        
+        public AudioClipVo GetSoundVoByType(EGameSoundFxType type) => gameSounds[type];
+        public AudioClipVo GetSoundVoByType(EUiSoundFxType type) => uiSounds[type];
+        public IReadOnlyList<AudioClipVo> GetBackgroundMusic(EGameSoundFxType type) => backgroundMusic;
+    }
+}
