91414022d5151a7ea0a2f857bf1b4febc3da0eaf
Kienan <69349336+KienanUnique@users.noreply.github.com>
2024-08-11 19:01:40 +0500

    TKM-137 add game title logo shaking logic

--- Assets/Scripts/Ui/MainMenu/MainMenuWindow.cs
+++ Assets/Scripts/Ui/MainMenu/MainMenuWindow.cs
@@ -1,6 +1,7 @@
 ﻿using Game.Ui.Parts.Sounds;
 using KoboldUi.Interfaces;
 using KoboldUi.Windows;
+using Ui.MainMenu.GameTitle;
 using Ui.MainMenu.MainMenu;
 using Ui.MainMenu.Ticker;
 using UnityEngine;
@@ -12,12 +13,14 @@ namespace Ui.MainMenu
         [SerializeField] private MainMenuView mainMenuView;
         [SerializeField] private SettingsView settingsView;
         [SerializeField] private TickerView tickerView;
+        [SerializeField] private GameTitleView gameTitleView;
         
         protected override void AddControllers()
         {
             AddController<ManMenuController, MainMenuView>(mainMenuView);
             AddController<SettingsController, SettingsView>(settingsView);
             AddController<TickerController, TickerView>(tickerView);
+            AddController<GameTitleController, GameTitleView>(gameTitleView);
         }
     }
 }
